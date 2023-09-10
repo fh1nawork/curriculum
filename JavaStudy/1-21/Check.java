552dd54c068e7f7266a3ed94a97c5e7b73477c17
fh1nawork <140414536+fh1nawork@users.noreply.github.com>
2023-09-10 20:36:10 +0900

    Update Check.java

--- JavaStudy/1-21/Check.java
+++ JavaStudy/1-21/Check.java
@@ -22,15 +22,15 @@ public class Check{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < 4-1; i++) {
-            for (int j = i+1; 4 > j; j--) {
+        for (int i = 0; i <  data.length -1; i++) {
+            for (int j = data.length -1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[i] > data[j]){
-                  int box = data[i];
-                  data[i] = data[j];
+                if(data[j-1] > data[j]){
+                  int box = data[j-1];
+                  data[j-1] = data[j];
                   data[j] = box;
                 }
             }
